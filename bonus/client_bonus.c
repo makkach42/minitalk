9d63dc39b8eb5d0d177dd78ddf2e84741e47dc24
makkach <makkach@e3r7p17.1337.ma>
2025-03-11 17:17:00 +0000

    minitalk

--- bonus/client_bonus.c
+++ bonus/client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: makkach <makkach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:23:02 by makkach           #+#    #+#             */
-/*   Updated: 2025/03/10 17:36:56 by makkach          ###   ########.fr       */
+/*   Updated: 2025/03/11 17:16:32 by makkach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,25 @@ static int	ft_atoi(const char *str)
 	int		sign;
 	long	number;
 	int		i;
+	int		check;
 
 	sign = 1;
 	number = 0;
 	i = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign = -1;
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (number > (((9223372036854775807) - (str[i] - '0')) / 10))
-			return (-1);
+		check = number;
 		number = number * 10 + (str[i] - '0');
+		if ((number / 10) != check)
+			return (-1);
 		i++;
 	}
 	return ((int)number * sign);
